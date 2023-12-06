289f4cf83dee8fceddd9fddbad63be03d170c588
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2023-12-07 00:28:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ API_ID = int(getenv("API_ID", "10284859"))
 API_HASH = getenv("API_HASH", "b0ad58eb8b845ba0003e0d9ce5fc2196")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN","6009759960:AAGdkktajUIrw3jBn9UPqkpNTgGAY5eJ9dA")
+BOT_TOKEN = getenv("BOT_TOKEN","6009759960:AAEXoV1tVEQjNg2kwApWlYe-7oPx7SNIBzw")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://monivps5:monivps5@cluster0.kmbq8we.mongodb.net/?retryWrites=true&w=majority")
