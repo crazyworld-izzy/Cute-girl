e77be9b76b139b039cf29baef9bd86219138fea6
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2023-12-17 16:29:14 +0530

    Update gban.py

--- InflexMusic/plugins/sudo/gban.py
+++ InflexMusic/plugins/sudo/gban.py
@@ -21,7 +21,7 @@ from InflexMusic.utils.extraction import extract_user
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["gban", "globalban"]) & SUDOERS)
+@app.on_message(filters.command(["cgban", "cgban"]) & SUDOERS)
 @language
 async def global_ban(client, message: Message, _):
     if not message.reply_to_message:
